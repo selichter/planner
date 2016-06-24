ee4f0cc75b5addec4f9cc2f067ffda21a29ae83e
selichter <lichter.sarahe@gmail.com>
2016-06-23 20:57:08 -0500

    added task model, controller, route, and association with lists

---
+++ db/migrate/20160624013159_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :item
+      t.integer :list_id
+      t.date :due_date
+      t.boolean :finished
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624011513) do
+ActiveRecord::Schema.define(version: 20160624013159) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "item"
+    t.integer  "list_id"
+    t.date     "due_date"
+    t.boolean  "finished"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
