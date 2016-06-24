9d45e585706a1e1499e2d8b4754dfaabe5331ded
selichter <lichter.sarahe@gmail.com>
2016-06-23 20:27:28 -0500

    added lists resource and association to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623122801) do
+ActiveRecord::Schema.define(version: 20160624011513) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
